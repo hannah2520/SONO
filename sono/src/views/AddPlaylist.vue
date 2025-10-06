e383756a5728ba238848c06f2f03319052d28046
Jamar Fordham Jr <saintdawg@Jamars-MacBook-Pro.local>
2025-10-06 12:28:11 -0400

    feat: add AddPlaylist view to match router import

---
+++ sono/src/views/AddPlaylist.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view-add-playlist">
+    <AddPlaylist />
+  </div>
+</template>
+
+<script setup>
+import AddPlaylist from '../components/AddPlaylist.vue'
+</script>
+
+<style scoped>
+.view-add-playlist {
+  padding: 1rem;
+}
+</style>
