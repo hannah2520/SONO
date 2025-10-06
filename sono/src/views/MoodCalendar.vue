5c0f2c5f9d6d7b94795a6c9585e58e2984f12ad5
Jamar Fordham Jr <saintdawg@Jamars-MacBook-Pro.local>
2025-10-06 01:30:32 -0400

    feat: add MoodCalendar view and route

---
+++ sono/src/views/MoodCalendar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view-mood-calendar">
+    <MoodCalendar />
+  </div>
+</template>
+
+<script setup>
+import MoodCalendar from '../components/MoodCalendar.vue'
+</script>
+
+<style scoped>
+.view-mood-calendar {
+  padding: 1rem;
+}
+</style>
